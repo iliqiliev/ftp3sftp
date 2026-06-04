b5e92b556eeeb04ae194430b69c59c220b28dd1d
Iliya Iliev <iliq0000@proton.me>
2026-06-04 15:12:10 +0300

    big refactor

--- src/ftp2sftp/__main__.py
+++
@@ -1,4 +0,0 @@
-from .ftp2sftp import main
-
-if __name__ == "__main__":
-    main()
--- src/ftp3sftp/ftp3sftp.py
+++ src/ftp3sftp/ftp3sftp.py
@@ -1,30 +1,30 @@
-# ftp2sftp.py - A FTP to SFTP bridge
+# ftp3sftp.py - A FTP to SFTP bridge
 # Copyright (C) 2023  Sebastian Meyer <sparrow.242.de@gmail.com>
+# Copyright (C) 2026  Iliya Iliev     <iliq0000@proton.me>
 # You should have received a copy of the GNU General Public License V3
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
 import logging
-import paramiko
 import pathlib
-
 from collections import namedtuple
 from datetime import datetime as DateTime
 from datetime import timezone
 from logging.handlers import TimedRotatingFileHandler
+from stat import S_ISDIR
+from textwrap import dedent
+
+import paramiko
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-from stat import S_ISDIR
-
-from ftp2sftp.__about__ import version
 
 Address = namedtuple("Address", ("host", "port", "username", "password", "path"))
 
 
 def connect_sftp(host, port, username, password):
     """Connect a SFTP server, returns (transport, SFTP-Client)
-    
+
     This functions connects to a SFTP Server and returs a tuple with
     a paramiko/SSH-transport object and an SFTP-Client object.
 
@@ -55,12 +55,17 @@ def connect_sftp(host, port, username, password):
 
 
 class Authorizer(DummyAuthorizer):
-    """ Subclass of the dummy example to have the option to
+    """Subclass of the dummy example to have the option to
     manage users without a local directory on the filesystem"""
 
     def add_user(
-        self, username, password, homedir, perm='elr',
-        msg_login="Login successful.", msg_quit="Goodbye."
+        self,
+        username,
+        password,
+        homedir,
+        perm="elr",
+        msg_login="Login successful.",
+        msg_quit="Goodbye.",
     ):
         """Add a user to the virtual users table.
 
@@ -68,37 +73,36 @@ class Authorizer(DummyAuthorizer):
         on the local filesystem.
         """
         if self.has_user(username):
-            raise ValueError('user %r already exists' % username)
+            raise ValueError("user %r already exists" % username)
         self._check_permissions(username, perm)
         self.user_table[username] = {
-            'pwd': str(password),
-            'perm': perm,
-            'operms': {},
-            'msg_login': str(msg_login),
-            'msg_quit': str(msg_quit),
-            'home': homedir
+            "pwd": str(password),
+            "perm": perm,
+            "operms": {},
+            "msg_login": str(msg_login),
+            "msg_quit": str(msg_quit),
+            "home": homedir,
         }
 
 
 class FTP2SFTPHandler(FTPHandler):
-
     # this parameter has to be set from outside before the
     # initialization through the server.
     # This kind of monkey patching is more stable than also
     # subclassing the server only to pass this information.
     sftp_config = None
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    
+
     def handle_close(self):
-        logging.debug(f"call handle_close")
-        self.fs.close_sftp_connection()
+        logging.debug("call handle_close")
+        if self.fs is not None:
+            self.fs.close_sftp_connection()
         super().handle_close()
 
 
-class SFTPConnectedFS():
-    
+class SFTPConnectedFS:
     def __init__(self, home, handler):
         self.handler = handler
         self._root = home
@@ -106,8 +110,10 @@ class SFTPConnectedFS():
         self._cwd = self._root
         self._sftp_root = handler.sftp_config["basedir"] or "/"
         self.ssh_transport, self.sftp_client = connect_sftp(
-            handler.sftp_config["host"], handler.sftp_config["port"],
-            handler.sftp_config["username"], handler.sftp_config["password"]
+            handler.sftp_config["host"],
+            handler.sftp_config["port"],
+            handler.sftp_config["username"],
+            handler.sftp_config["password"],
         )
 
     def chdir(self, path):
@@ -117,7 +123,7 @@ class SFTPConnectedFS():
         self.cwd = self.fs2ftp(new_path)
 
     def close_sftp_connection(self):
-        logging.debug(f"call close_sftp_connection")
+        logging.debug("call close_sftp_connection")
         self.ssh_transport.close()
 
     @property
@@ -133,7 +139,7 @@ class SFTPConnectedFS():
         new_cwd_path = pathlib.Path(path).resolve()
         new_cwd = new_cwd_path.as_posix()
         if new_cwd.startswith(new_cwd_path.drive):
-            new_cwd = new_cwd[len(new_cwd_path.drive):]
+            new_cwd = new_cwd[len(new_cwd_path.drive) :]
         logging.debug(f"call set cwd : {path} -> {new_cwd}")
         self._cwd = new_cwd
 
@@ -144,7 +150,7 @@ class SFTPConnectedFS():
         for entry in self.sftp_client.listdir_iter(basedir):
             values = []
             if "type" in facts:
-                values.append(f"type=dir" if S_ISDIR(entry.st_mode) else f"type=file")
+                values.append("type=dir" if S_ISDIR(entry.st_mode) else "type=file")
             if "perm" in facts:
                 values.append("perm=r")
             if "size" in facts:
@@ -161,7 +167,11 @@ class SFTPConnectedFS():
     def fs2ftp(self, fspath):
         if fspath.startswith("."):
             fspath = fspath[1:]
-        fspath = f"{fspath[len(self._sftp_root):]}" if fspath.startswith(self._sftp_root) else fspath
+        fspath = (
+            f"{fspath[len(self._sftp_root) :]}"
+            if fspath.startswith(self._sftp_root)
+            else fspath
+        )
         ftppath = f"{self._root.rstrip('/')}/{fspath.lstrip('/')}"
         logging.debug(f"call fs2ftp : {fspath} -> {ftppath}")
         return ftppath
@@ -169,7 +179,11 @@ class SFTPConnectedFS():
     def ftp2fs(self, ftppath):
         if not ftppath.startswith("/"):
             ftppath = f"{self._cwd}/{ftppath}"
-        fspath = f"{ftppath[len(self._root):]}" if ftppath.startswith(self._root) else ftppath
+        fspath = (
+            f"{ftppath[len(self._root) :]}"
+            if ftppath.startswith(self._root)
+            else ftppath
+        )
         fspath = f"{self._sftp_root.rstrip('/')}/{fspath.lstrip('/')}"
         logging.debug(f"call ftp2fs : {ftppath} -> {fspath}")
         return fspath
@@ -190,7 +204,7 @@ class SFTPConnectedFS():
             is_dir = S_ISDIR(stat.st_mode)
         logging.debug(f"call isdir : {path} -> {is_dir}")
         return is_dir
-    
+
     def isfile(self, path):
         is_file = None
         try:
@@ -207,7 +221,7 @@ class SFTPConnectedFS():
         try:
             _ = self.sftp_client.stat(path)
         except FileNotFoundError:
-            exists =  False
+            exists = False
         logging.debug(f"call lexists : {path} -> {exists}")
         return exists
 
@@ -215,7 +229,7 @@ class SFTPConnectedFS():
         listing = self.sftp_client.listdir(path)
         logging.debug(f"call listdir : {path} -> {listing}")
         return listing
-    
+
     def mkdir(self, path):
         logging.debug(f"call mkdir : {path}")
         self.sftp_client.mkdir(path)
@@ -226,7 +240,7 @@ class SFTPConnectedFS():
         # the .name property of the handle is used by the FTP library.
         sftp_open.name = filename
         return sftp_open
-    
+
     def realpath(self, path):
         if not path.startswith("/"):
             cwd = self._cwd[:-1] if self._cwd.endswith("/") else self._cwd
@@ -266,8 +280,8 @@ class SFTPConnectedFS():
 
 def setup_logger(loglevel, logdir, keeplog):
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
     logger = logging.getLogger()
     logger.setLevel(logging.getLevelName(loglevel))
@@ -277,9 +291,12 @@ def setup_logger(loglevel, logdir, keeplog):
     logger.addHandler(stream_handler)
     logger.info(f"Logging is active on level {logging.getLevelName(logger.level)}")
     if logdir:
-        logfile = logdir / "ftp2sftp_log.txt"
+        logfile = logdir / "ftp3sftp_log.txt"
         handler = TimedRotatingFileHandler(
-            logfile, when="d", interval=1, backupCount=keeplog,
+            logfile,
+            when="d",
+            interval=1,
+            backupCount=keeplog,
         )
         handler.setLevel(logging.getLevelName(loglevel))
         handler.setFormatter(formatter)
@@ -296,42 +313,65 @@ def parse_address(address):
 
 
 def parse_arguments_options():
-    parser = argparse.ArgumentParser(fromfile_prefix_chars='@',
+    parser = argparse.ArgumentParser(
+        fromfile_prefix_chars="@",
     )
-    parser.add_argument("--ftp", type=parse_address, required=True,
-        help="Information where to start the FTP server: username:password@host:port:/homedir"
+    parser.add_argument(
+        "--ftp",
+        type=parse_address,
+        required=True,
+        help="Information where to start the FTP server: username:password@host:port:/homedir",
     )
-    parser.add_argument("--sftp", type=parse_address, required=True,
-        help="Information how to connect the SFTP server: username:password@host:port:/homedir"
+    parser.add_argument(
+        "--sftp",
+        type=parse_address,
+        required=True,
+        help="Information how to connect the SFTP server: username:password@host:port:/homedir",
     )
-    parser.add_argument("--loglevel", type=str, default="INFO",
+    parser.add_argument(
+        "--loglevel",
+        type=str,
+        default="INFO",
         choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET"],
-        help="Set the loglevel for logging. (Default: INFO)"
+        help="Set the loglevel for logging. (Default: INFO)",
     )
-    parser.add_argument("--logdir", type=pathlib.Path, default=None,
-        help="Directory to write the log. Logs will be sperated in files daily"
+    parser.add_argument(
+        "--logdir",
+        type=pathlib.Path,
+        default=None,
+        help="Directory to write the log. Logs will be sperated in files daily",
     )
-    parser.add_argument("--keeplog", type=int, default=7,
-        help="How long (in days) keep the logfile if logdir is set (Default: 7)"
+    parser.add_argument(
+        "--keeplog",
+        type=int,
+        default=7,
+        help="How long (in days) keep the logfile if logdir is set (Default: 7)",
     )
     args = parser.parse_args()
     return args
 
 
 def main():
-    print(f"ftp2sftp.py (Version {version})")
-    print("Copyright (C) 2023  Sebastian Meyer <sparrow.242.de@gmail.com>")
-    print("Licensed under GNU GPL (https://www.gnu.org/licenses/gpl-3.0.html)")
-    print("This program comes with ABSOLUTELY NO WARRANTY.")
-    print("This is free software, and you are welcome to redistribute it")
-    print("under certain conditions.")
+    print(
+        dedent("""\
+        ftp3sftp.py (Version 0.2)
+        Copyright (C) 2023  Sebastian Meyer <sparrow.242.de@gmail.com>
+        Copyright (C) 2026  Iliya Iliev   <iliq0000@proton.me>
+        Licensed under GNU GPL (https://www.gnu.org/licenses/gpl-3.0.html)
+        This program comes with ABSOLUTELY NO WARRANTY.
+        This is free software, and you are welcome to redistribute it
+        under certain conditions.""")
+    )
+
     options = parse_arguments_options()
     setup_logger(options.loglevel, options.logdir, options.keeplog)
     logging.info("fpt2sftp is starting...")
     authorizer = Authorizer()
     authorizer.add_user(
-        options.ftp.username, options.ftp.password, perm="elradfmwMT",
-        homedir = options.ftp.path or f"/home/{options.ftp.username}"
+        options.ftp.username,
+        options.ftp.password,
+        perm="elradfmwMT",
+        homedir=options.ftp.path or f"/home/{options.ftp.username}",
     )
     handler = FTP2SFTPHandler
     handler.authorizer = authorizer
@@ -341,7 +381,7 @@ def main():
         "port": options.sftp.port,
         "username": options.sftp.username,
         "password": options.sftp.password,
-        "basedir": options.sftp.path
+        "basedir": options.sftp.path,
     }
     handler.sftp_config = sftp_config
     server = FTPServer((options.ftp.host, options.ftp.port), handler)
