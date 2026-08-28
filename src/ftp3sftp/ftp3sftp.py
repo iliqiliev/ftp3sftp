5f909efe2883a935b962069f119959648925c591
Iliya Iliev <iliq0000@proton.me>
2026-08-28 15:47:24 +0300

    meta: import future annotations

--- src/ftp3sftp/ftp3sftp.py
+++ src/ftp3sftp/ftp3sftp.py
@@ -4,6 +4,8 @@
 # You should have received a copy of the GNU General Public License V3
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from __future__ import annotations
+
 import argparse
 import logging
 import pathlib
